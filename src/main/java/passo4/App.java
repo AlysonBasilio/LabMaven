bd4147beb9c09bd5c8c6a2ecbb1d7fd8815925bf
AlysonBasilio <alyson.basilio@gmail.com>
2017-04-21 16:58:16 -0300

    Passo 4 completo

---
+++ src/main/java/passo4/App.java
@@ -0,0 +1,12 @@
+package passo4;
+
+import com.google.gson.Gson;
+
+public class App {
+
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		System.out.println(gson.toJson("Hello World!"));
+	}
+
+}
